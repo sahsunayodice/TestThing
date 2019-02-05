33685bc2bb2c4c67214820b2f9951571cdc8aa99
sahsunayodice <shauna.yodice@gmail>
2019-02-05 09:47:28 -0500

    first commit

--- src/NameOfCode.java
+++ src/NameOfCode.java
@@ -2,6 +2,7 @@ public class NameOfCode {
     public static void main(String[]args){
         System.out.println("Hello");
         System.out.println("my name is shawana");
+        System.out.println("and my eye is itchy");
     }
 
 }
